e5a2316b3a389ab6f2c35d10fb051232d0cfec2c
Julio238 <julio.cesarnascimentodos.santos@escola.pr.gov.br>
2026-08-14 09:14:58 -0300

    Create bancoJson.sql

---
+++ bancoJson.sql
@@ -0,0 +1,11 @@
+CREATE TABLE produtos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50),
+    detalhes JSON
+);
+
+VALUES ('Camiseta', '{"cores": ["vermelho", "azul"], "tamanhos": ["P", "M", "G"]}');
+
+
+SELECT JSON_EXTRACT(detalhes, '$.cores') AS cores FROM produtos;
+WHERE JSON_CONTAINS(detalhes, '"vermelho"', '$.cores');
